2533329b7eee317c01e0941ef6e11c3833de2d52
eniompw <71254710+eniompw@users.noreply.github.com>
2025-07-25 17:24:25 +0000

    Fix download script to create tok128 directory and update file names for TinyStories dataset

--- download_tinystories.sh
+++ download_tinystories.sh
@@ -1,13 +1,13 @@
-mkdir -p ./modded-llama2.c/data
+mkdir -p ./modded-llama2.c/data/tok128
 cd ./modded-llama2.c/data
 # Download files in parallel directly into the target directory
 wget https://huggingface.co/datasets/enio/TinyStories/resolve/main/tok128/tok128.model & \
 wget https://huggingface.co/datasets/enio/TinyStories/raw/main/tok128/tok128.vocab & \
-wget https://huggingface.co/datasets/enio/TinyStories/resolve/main/tok128/tok128.tar.gz & \
+wget https://huggingface.co/datasets/enio/TinyStories/resolve/main/tok128/tok128-01.tar.gz & \
 wait
 
 # Untar the large file after all downloads are complete
-tar -I pigz -xvf tok128.tar.gz
+tar -I pigz -xvf tok128-01.tar.gz -C ./tok128/
 
 # compile run / inference executable
 cd .. && gcc -O3 -o run run.c -lm
